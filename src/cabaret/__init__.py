c872b81f7ec0e15cc422853819d2e8a891ea9095
dgegen <ddegen@ethz.ch>
2025-09-24 18:53:51 +0200

    Add Filters to __init__.py

--- src/cabaret/__init__.py
+++ src/cabaret/__init__.py
@@ -2,12 +2,14 @@ from cabaret.camera import Camera
 from cabaret.focuser import Focuser
 from cabaret.image import generate_image
 from cabaret.observatory import Observatory
+from cabaret.queries import Filters
 from cabaret.site import Site
 from cabaret.sources import Sources
 from cabaret.telescope import Telescope
 
 __all__ = [
     "Camera",
+    "Filters",
     "Focuser",
     "generate_image",
     "Observatory",
