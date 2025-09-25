e335e0973ee42e5350b0467dd36be0e173d6fcff
dgegen <ddegen@ethz.ch>
2025-09-25 10:49:46 +0200

    Improve plot_image

--- src/cabaret/plot.py
+++ src/cabaret/plot.py
@@ -15,13 +15,34 @@ def plot_image(
     ax=None,
     add_colorbar=True,
     colorbar_kwargs={},
+    transparent=True,
 ):
-    """Plot a 2D image with zscale normalization."""
+    """Plot a 2D image with zscale normalization.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        The image to plot.
+    title : str, optional
+        Title for the plot.
+    cmap : str, optional
+        Colormap to use.
+    ax : matplotlib.axes.Axes, optional
+        Axes to plot on.
+    add_colorbar : bool, optional
+        Whether to add a colorbar.
+    colorbar_kwargs : dict, optional
+        Additional kwargs for colorbar.
+    transparent : bool, optional
+        If True, set figure background to transparent.
+    """
     if ax is None:
-        _, ax = plt.subplots()
+        fig, ax = plt.subplots()
+    else:
+        fig = plt.gcf()
     interval = ZScaleInterval()
     vmin, vmax = interval.get_limits(image)
-    img = ax.imshow(image, vmin=vmin, vmax=vmax, cmap=cmap)
+    img = ax.imshow(image, vmin=vmin, vmax=vmax, cmap=cmap, rasterized=True)
     if title is not None:
         ax.set_title(title)
     ax.set_xlabel("Pixels")
@@ -32,5 +53,9 @@ def plot_image(
             img, ax=ax, **{"fraction": 0.046, "pad": 0.04} | colorbar_kwargs
         )
         cbar.set_label("ADU")
+
+    if transparent:
+        fig.patch.set_alpha(0)
+        ax.patch.set_alpha(0)
 
     return img
